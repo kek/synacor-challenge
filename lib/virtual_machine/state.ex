fc42c2ffca0d51d56a74967874f887a2726a2720
Karl Eklund <localpart@gmail.com>
2018-04-19 23:42:06 +0200

    Initialize registers to 0

--- lib/virtual_machine/state.ex
+++ lib/virtual_machine/state.ex
@@ -1,5 +1,14 @@
 defmodule VirtualMachine.State do
-  defstruct registers: %{},
+  defstruct registers: %{
+              32768 => 0,
+              32769 => 0,
+              32770 => 0,
+              32771 => 0,
+              32772 => 0,
+              32773 => 0,
+              32774 => 0,
+              32775 => 0
+            },
             memory: [],
             output: VirtualMachine.Terminal,
             stack: [],
